a2d0f17fd757856d275d35ce2461d336ea06607c
AdamBadagliacco <48744499+AdamBadagliacco@users.noreply.github.com>
2021-01-06 14:10:42 -0600

    Uploaded source and pom files

---
+++ src/main/java/com/sg/classroster/entity/Course.java
@@ -0,0 +1,100 @@
+package com.sg.classroster.entity;
+
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Course {
+	
+	private int id;
+	
+	@NotBlank(message = "Name must not be blank")
+	@Size(max = 50, message="Name must be fewer than 50 characters")
+	private String name;
+
+	@Size(max = 255, message = "Description must be fewer than 255 characters")
+	private String description;
+
+	private Teacher teacher;
+	
+	//Can be classes without students (Ex. Class is created but students have yet to enroll)
+	private List<Student> students;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Teacher getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+	public List<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((students == null) ? 0 : students.hashCode());
+		result = prime * result + ((teacher == null) ? 0 : teacher.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (students == null) {
+			if (other.students != null)
+				return false;
+		} else if (!students.equals(other.students))
+			return false;
+		if (teacher == null) {
+			if (other.teacher != null)
+				return false;
+		} else if (!teacher.equals(other.teacher))
+			return false;
+		return true;
+	}
+
+}
